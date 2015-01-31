9184c270cce374710e05c095fbc627958b359063
Wolfgang Imig <wolfgang.imig@googlemail.com>
2015-01-31 11:34:54 +0100

    re-compiled

--- itol/com.wilutions.itol.IssueApplication.bat
+++ itol/com.wilutions.itol.IssueApplication.bat
@@ -1,6 +1,6 @@
-pushd "D:\git\itol\itol"
-set CP=D:\git\itol\itol\bin
-set CP=%CP%;D:\git\itol\itol-db\bin
+pushd "d:\git\itol\itol"
+set CP=d:\git\itol\itol\bin
+set CP=%CP%;d:\git\itol\itol-db\bin
 set CP=%CP%;D:\git\joa\java\lib
 set CP=%CP%;D:\git\joa\java\lib\joa.jar
-"C:\Program Files\Java\jre1.8.0_20\bin\java.exe" -classpath "%CP%" com.wilutions.itol.IssueApplication
+"D:\Program Files (x86)\Java\jdk1.8.0_20\jre\bin\java.exe" -classpath "%CP%" com.wilutions.itol.IssueApplication
