43ae46c013749bf711fd710d33241211bc5186e1
Wolfgang Imig <wolfgang.imig@googlemail.com>
2015-01-31 12:25:25 +0100

    Added copyright.

--- itol/IssueServiceImpl.js
+++ itol/IssueServiceImpl.js
@@ -1,3 +1,13 @@
+/*
+    Copyright (c) 2015 Wolfgang Imig
+    
+    This file is part of the library "JOA Issue Tracker for Microsoft Outlook".
+
+    This file must be used according to the terms of   
+      
+      MIT License, http://opensource.org/licenses/MIT
+
+ */
 //load("nashorn:mozilla_compat.js");
 //importPackage(com.wilutions.itol.db);
 
--- itol/src/com/wilutions/itol/DlgProgress.java
+++ itol/src/com/wilutions/itol/DlgProgress.java
@@ -1,3 +1,13 @@
+/*
+    Copyright (c) 2015 Wolfgang Imig
+    
+    This file is part of the library "JOA Issue Tracker for Microsoft Outlook".
+
+    This file must be used according to the terms of   
+      
+      MIT License, http://opensource.org/licenses/MIT
+
+ */
 package com.wilutions.itol;
 
 import java.io.IOException;
