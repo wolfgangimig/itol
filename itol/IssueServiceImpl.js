ac592d1a251c4f54e5e97119d3be76755377ff94
Imig <w.imig@elo.com>
2015-02-16 14:03:43 +0100

    getShowIssueUrl auf getIssueHistoryUrl zurückgeführt. NPE vermeiden, wenn Outlook setMailItem aufruft, bevor TaskPane initialisiert ist.

--- itol/IssueServiceImpl.js
+++ itol/IssueServiceImpl.js
@@ -809,7 +809,7 @@ function getHtmlEditor(issue, propertyId) {
 }
 
 function getShowIssueUrl(issueId) {
-	return config.url + "/issues/" + issueId;
+	return getIssueHistoryUrl(issueId);
 }
 
 function getMsgFileType() {
