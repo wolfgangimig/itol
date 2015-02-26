d92f9654a42556e09d6ba9f86de3c1a79500b817
Wolfgang Imig <wolfgang.imig@googlemail.com>
2015-02-26 22:24:30 +0100

    Version 1.2

--- itol/com.wilutions.itol.IssueApplication.bat
+++ itol/com.wilutions.itol.IssueApplication.bat
@@ -2,4 +2,4 @@ pushd "d:\git\itol\itol"
 set CP=d:\git\itol\itol\bin
 set CP=%CP%;d:\git\itol\itol-db\bin
 set CP=%CP%;D:\git\joa\java\joa\bin
-"D:\Program Files (x86)\Java\jdk1.8.0_20\jre\bin\java.exe" -classpath "%CP%" com.wilutions.itol.IssueApplication
+"D:\Program Files (x86)\Java\jdk1.8.0_31\jre\bin\java.exe" -classpath "%CP%" com.wilutions.itol.IssueApplication
