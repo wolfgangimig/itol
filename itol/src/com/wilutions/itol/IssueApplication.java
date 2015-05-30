48f5110f7eb376ae3db26025b48f37e1da9f7deb
Wolfgang Imig <wolfgang.imig@googlemail.com>
2015-05-30 12:40:08 +0200

    Try to create link in autostart - but missing changes made on other computer in JOA DLL

--- itol/src/com/wilutions/itol/IssueApplication.java
+++ itol/src/com/wilutions/itol/IssueApplication.java
@@ -20,19 +20,20 @@ import java.util.logging.Logger;
 import javafx.stage.Stage;
 
 import com.wilutions.com.ComException;
+import com.wilutions.com.JoaDll;
 import com.wilutions.com.reg.RegUtil;
 import com.wilutions.itol.db.Property;
 import com.wilutions.itol.db.impl.IssueServiceFactory_JS;
 import com.wilutions.joa.AddinApplication;
 
-
 public class IssueApplication extends AddinApplication {
-	
+
 	private static Logger log = Logger.getLogger(IssueApplication.class.getName());
 
 	static {
 		Config config = Globals.getConfig();
-		//config.appName = "Issue Tracker for Microsoft Outlook " + System.getProperty("sun.arch.data.model") + "bit";
+		// config.appName = "Issue Tracker for Microsoft Outlook " +
+		// System.getProperty("sun.arch.data.model") + "bit";
 		config.appName = "Issue Tracker for Microsoft Outlook and Redmine";
 		config.manufacturerName = "WILUTIONS";
 		config.serviceFactoryClass = IssueServiceFactory_JS.class.getName();
@@ -73,14 +74,14 @@ public class IssueApplication extends AddinApplication {
 	}
 
 	public static void main(String[] args) {
-		
+
 		Globals.initLogging();
 		String javaHome = System.getProperty("java.home");
 		log.info("java.home=" + javaHome);
 		for (Object key : System.getProperties().keySet()) {
-			log.info(key + "=" + System.getProperty((String)key));
+			log.info(key + "=" + System.getProperty((String) key));
 		}
-		
+
 		log.info("app.dir=" + RegUtil.getAppPathIfSelfContained());
 
 		try {
@@ -90,9 +91,9 @@ public class IssueApplication extends AddinApplication {
 		catch (Throwable e) {
 			log.log(Level.SEVERE, "Failed to excecute main.", e);
 		}
-		
+
 		log.info("main finished");
-		
+
 		Globals.releaseResources();
 	}
 
@@ -110,49 +111,24 @@ public class IssueApplication extends AddinApplication {
 			instance = this;
 			super.register(userNotMachine, execPath);
 			
-//		      String exe = RegUtil.getExecPath(RemoteTestMain.class);
-//		      if (exe.startsWith("\"")) exe = exe.substring(1);
-//		      if (exe.endsWith("\"")) exe = exe.substring(0, exe.length()-1);
-//		      String linkName = makeValidPath(AUTOSTART_FOLDER + "\\" + (new File(exe)).getName() + ".lnk");
-//		      if (log.isDebugEnabled()) log.debug("linkName=" + linkName);
-//		      
-//		      // Im Autostart-Ordner ablegen
-//		      if (arg.equals("/Register")) {
-//		        try {
-//		          String targetName = exe;
-//		          String description = "";
-//		          if (log.isInfoEnabled()) log.info("Create shortcut=" + linkName + " to " + targetName);
-//		          JoaDll.nativeCreateShortcut(linkName, targetName, description);
-//		        } catch (Exception e) {
-//		          log.error("Failed to create shortcut=" + linkName, e);
-//		        }
-//		      }
-//		      
-//		      // Im Autostart-Ordner löschen.
-//		      else if (arg.equals("/Unregister")) {
-//		        log.info("Delete lnk=" + linkName);
-//		        new File(linkName).delete();
-//		      }
-			
-//			pushd "%APPDATA%\..\Local\Issue Tracker for Microsoft Outlook 32bit"
-//			REG add "HKCU\Software\Microsoft\Office\Outlook\Addins\ItolAddin.Class" /f /v "LoadBehavior" /t REG_DWORD /d 3
-//			START "" "Issue Tracker for Microsoft Outlook 32bit.exe"
-//			popd
+			registerAutostart(true);
 
-			
-			//showDocument("http://www.wilutions.com/joa/itol/installed.html");
+			// showDocument("http://www.wilutions.com/joa/itol/installed.html");
 		}
 		catch (Throwable e) {
 			log.log(Level.SEVERE, "Failed to register Addin", e);
 		}
 	}
-	
+
 	@Override
 	protected void unregister(boolean userNotMachine) {
 		try {
 			instance = this;
+			
+			registerAutostart(false);
+
 			super.unregister(userNotMachine);
-			//showDocument("http://www.wilutions.com/joa/itol/uninstalled.html");
+			// showDocument("http://www.wilutions.com/joa/itol/uninstalled.html");
 		}
 		catch (Throwable e) {
 			log.log(Level.SEVERE, "Failed to register Addin", e);
@@ -164,4 +140,68 @@ public class IssueApplication extends AddinApplication {
 	public static void showDocument(String url) {
 		instance.getHostServices().showDocument(url);
 	}
+
+	private void registerAutostart(boolean registerNotUnregister) {
+		String exe = RegUtil.getExecPath(IssueApplication.class);
+		if (exe.startsWith("\"")) exe = exe.substring(1);
+		if (exe.endsWith("\"")) exe = exe.substring(0, exe.length() - 1);
+		String linkName = makeValidPath(AUTOSTART_FOLDER + "\\" + (new File(exe)).getName() + ".lnk");
+		if (log.isLoggable(Level.FINE)) log.log(Level.FINE, "linkName=" + linkName);
+
+		// Im Autostart-Ordner ablegen
+		if (registerNotUnregister) {
+			try {
+				String targetName = exe;
+				String description = "";
+				if (log.isLoggable(Level.INFO)) log
+						.log(Level.INFO, "Create shortcut=" + linkName + " to " + targetName);
+				
+				// MISSING
+				JoaDll.nativeCreateShortcut(linkName, targetName, description);
+			}
+			catch (Throwable e) {
+				log.log(Level.SEVERE, "Failed to create shortcut=" + linkName, e);
+			}
+		}
+
+		// Im Autostart-Ordner löschen.
+		else {
+			if (log.isLoggable(Level.INFO)) log.log(Level.INFO, "Delete lnk=" + linkName);
+			new File(linkName).delete();
+		}
+
+		// pushd "%APPDATA%\..\Local\Issue Tracker for Microsoft Outlook 32bit"
+		// REG add
+		// "HKCU\Software\Microsoft\Office\Outlook\Addins\ItolAddin.Class" /f /v
+		// "LoadBehavior" /t REG_DWORD /d 3
+		// START "" "Issue Tracker for Microsoft Outlook 32bit.exe"
+		// popd
+
+	}
+
+	/**
+	 * Make valid path.
+	 * 
+	 * @param path
+	 *            File system path that might contain environment variables.
+	 * @return File system path with replaced variables.
+	 */
+	public static String makeValidPath(String path) {
+
+		int p = path.indexOf('%');
+		while (p >= 0) {
+
+			int e = path.indexOf('%', p + 1);
+			String variableName = path.substring(p + 1, e);
+			String variableValue = System.getenv(variableName);
+			if (variableValue == null) variableValue = "";
+			path = path.replace(path.substring(p, e + 1), variableValue);
+
+			p = path.indexOf('%');
+		}
+
+		return path;
+	}
+	
+	private final static String AUTOSTART_FOLDER = "%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup";
 }
