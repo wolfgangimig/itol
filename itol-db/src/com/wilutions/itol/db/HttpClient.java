b0f3ef56b4dd425d4d71d2433ec900fc009a7225
Wolfgang Imig <wolfgang.imig@googlemail.com>
2016-12-30 13:58:58 +0100

    Removed obsolete import

--- itol-db/src/com/wilutions/itol/db/HttpClient.java
+++ itol-db/src/com/wilutions/itol/db/HttpClient.java
@@ -19,7 +19,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.InterruptedIOException;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
