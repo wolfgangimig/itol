a19b8600b2cc9dde0e84906a9fa26c0af7f67ea9
Imig <Imig@PCIMIGW.ELO.local>
2017-01-10 14:17:32 +0100

    Classpath changed after gradle build

--- itol-addin/com.wilutions.itol.IssueApplication.bat
+++ itol-addin/com.wilutions.itol.IssueApplication.bat
@@ -5,4 +5,4 @@ set CP=%CP%;C:\Users\Wolfgang\.gradle\caches\modules-2\files-2.1\junit\junit\4.1
 set CP=%CP%;C:\Users\Wolfgang\.gradle\caches\modules-2\files-2.1\org.controlsfx\controlsfx\8.40.10\fe12544570095b334d45c61e8c9a26f30ffc1675\controlsfx-8.40.10.jar
 set CP=%CP%;C:\Users\Wolfgang\.gradle\caches\modules-2\files-2.1\org.hamcrest\hamcrest-core\1.3\42a25dc3219429f0e5d060061f71acb49bf010a0\hamcrest-core-1.3.jar
 set CP=%CP%;D:\git\joa\java\joa\bin
-"C:\Program Files (x86)\Java\jdk1.8.0_65\jre\bin\java.exe" -classpath "%CP%" com.wilutions.itol.IssueApplication
+"C:\Program Files (x86)\Java\jdk1.8.0_65\jre\bin\javaw.exe" -classpath "%CP%" com.wilutions.itol.IssueApplication
