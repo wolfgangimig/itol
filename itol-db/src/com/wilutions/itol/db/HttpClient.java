cabc78aff5c5eafefa28442b301b7cb86f6a8e44
wolfgangimig <wolfgang.imig@gmail.com>
2017-05-02 10:30:43 +0200

    Uncomment obsolete function that uses JRE internas

--- itol-db/src/com/wilutions/itol/db/HttpClient.java
+++ itol-db/src/com/wilutions/itol/db/HttpClient.java
@@ -407,35 +407,35 @@ public class HttpClient {
 
 	// http://stackoverflow.com/questions/6659360/how-to-solve-javax-net-ssl-sslhandshakeexception-error
 	// trusting all certificate
-	@SuppressWarnings("unused")
-	private static void doTrustToCertificates() throws Exception {
-		Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-			public X509Certificate[] getAcceptedIssuers() {
-				return null;
-			}
-
-			public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
-				return;
-			}
-
-			public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
-				return;
-			}
-		} };
-
-		SSLContext sc = SSLContext.getInstance("SSL");
-		sc.init(null, trustAllCerts, new SecureRandom());
-		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-		HostnameVerifier hv = new HostnameVerifier() {
-			public boolean verify(String urlHostName, SSLSession session) {
-				if (!urlHostName.equalsIgnoreCase(session.getPeerHost())) {
-					System.out.println("Warning: URL host '" + urlHostName + "' is different to SSLSession host '"
-							+ session.getPeerHost() + "'.");
-				}
-				return true;
-			}
-		};
-		HttpsURLConnection.setDefaultHostnameVerifier(hv);
-	}
+//	@SuppressWarnings("unused")
+//	private static void doTrustToCertificates() throws Exception {
+//		Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+//		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+//			public X509Certificate[] getAcceptedIssuers() {
+//				return null;
+//			}
+//
+//			public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
+//				return;
+//			}
+//
+//			public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
+//				return;
+//			}
+//		} };
+//
+//		SSLContext sc = SSLContext.getInstance("SSL");
+//		sc.init(null, trustAllCerts, new SecureRandom());
+//		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+//		HostnameVerifier hv = new HostnameVerifier() {
+//			public boolean verify(String urlHostName, SSLSession session) {
+//				if (!urlHostName.equalsIgnoreCase(session.getPeerHost())) {
+//					System.out.println("Warning: URL host '" + urlHostName + "' is different to SSLSession host '"
+//							+ session.getPeerHost() + "'.");
+//				}
+//				return true;
+//			}
+//		};
+//		HttpsURLConnection.setDefaultHostnameVerifier(hv);
+//	}
 }
