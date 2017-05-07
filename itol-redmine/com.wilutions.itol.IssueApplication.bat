aae176b52f8e75c8ceb3476adfbb1ba52e348b77
Wolfgang Imig <wolfgang.imig@googlemail.com>
2017-05-07 18:01:58 +0200

    Update BAT files, remove obsolete.

--- itol-redmine/com.wilutions.itol.IssueApplication.bat
+++
@@ -1,8 +0,0 @@
-pushd "D:\git\itol\itol-redmine"
-set CP=D:\git\itol\itol-redmine\bin
-set CP=%CP%;C:\Users\Wolfgang\.gradle\caches\modules-2\files-2.1\junit\junit\4.12\2973d150c0dc1fefe998f834810d68f278ea58ec\junit-4.12.jar
-set CP=%CP%;C:\Users\Wolfgang\.gradle\caches\modules-2\files-2.1\org.hamcrest\hamcrest-core\1.3\42a25dc3219429f0e5d060061f71acb49bf010a0\hamcrest-core-1.3.jar
-set CP=%CP%;D:\git\itol\itol-addin\bin
-set CP=%CP%;D:\git\itol\itol-addin\lib\joa.jar
-set CP=%CP%;D:\git\itol\itol-db\bin
-"C:\Program Files\Java\jdk1.8.0_71\jre\bin\java.exe" -classpath "%CP%" com.wilutions.itol.IssueApplication
--- itol-redmine/com.wilutions.redmineaddin.IssueApplication.bat
+++ itol-redmine/com.wilutions.redmineaddin.IssueApplication.bat
@@ -1,8 +1,8 @@
-pushd "D:\git\itol\itol-redmine"
-set CP=D:\git\itol\itol-redmine\bin
+pushd "D:\git\itol-redmine\itol\itol-redmine"
+set CP=D:\git\itol-redmine\itol\itol-redmine\bin
+set CP=%CP%;D:\git\itol-redmine\itol\itol-db\bin
+set CP=%CP%;D:\git\itol-redmine\itol\itol-addin\bin
+set CP=%CP%;D:\git\itol-redmine\itol\itol-addin\lib\joa.jar
 set CP=%CP%;C:\Users\Wolfgang\.gradle\caches\modules-2\files-2.1\junit\junit\4.12\2973d150c0dc1fefe998f834810d68f278ea58ec\junit-4.12.jar
 set CP=%CP%;C:\Users\Wolfgang\.gradle\caches\modules-2\files-2.1\org.hamcrest\hamcrest-core\1.3\42a25dc3219429f0e5d060061f71acb49bf010a0\hamcrest-core-1.3.jar
-set CP=%CP%;D:\git\itol\itol-addin\bin
-set CP=%CP%;D:\git\itol\itol-addin\lib\joa.jar
-set CP=%CP%;D:\git\itol\itol-db\bin
-"C:\Program Files\Java\jdk1.8.0_71\jre\bin\java.exe" -classpath "%CP%" com.wilutions.redmineaddin.IssueApplication
+"C:\Program Files\Java\jre1.8.0_71\bin\java.exe" -classpath "%CP%" com.wilutions.redmineaddin.IssueApplication
